b4778f157d16a21b42be43759d7c4ee76fe4918f
Guillaume R <theelectronwill@gmail.com>
2018-04-22 21:41:52 +0200

    Add convenience method to read a TagCompound from an InputStream

--- src/main/java/com/electronwill/nbj/TagCompound.java
+++ src/main/java/com/electronwill/nbj/TagCompound.java
@@ -3,6 +3,10 @@ package com.electronwill.nbj;
 import com.electronwill.nbj.tagtypes.TagType;
 import com.electronwill.nbj.tagtypes.Types;
 
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -48,5 +52,20 @@ public final class TagCompound extends Tag<Map<String, Object>> implements Itera
 				return new Tag<>(type, next.getKey(), next.getValue());
 			}
 		};
+	}
+
+	public static TagCompound read(DataInput input) throws IOException {
+		int typeId = input.readByte();
+		if (typeId != Types.COMPOUND.id()) {
+			throw new NbtException("Invalid first id in TagCompound, expected Compound's id, not " + typeId);
+		}
+		String name = Types.STRING.readValue(input);
+		Map<String, Object> values = Types.COMPOUND.readValue(input);
+		return new TagCompound(name, values);
+	}
+
+	public static TagCompound read(InputStream input) throws IOException {
+		DataInput dataInput = (input instanceof DataInput) ? (DataInput)input : new DataInputStream(input);
+		return read(dataInput);
 	}
 }
