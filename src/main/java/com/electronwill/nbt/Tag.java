5524e8a2061368e1023b74ccbef73e09fc0a8153
Guillaume R <theelectronwill@gmail.com>
2018-04-22 21:31:41 +0200

    Add Tag (any type) and TagCompound

---
+++ src/main/java/com/electronwill/nbt/Tag.java
@@ -0,0 +1,47 @@
+package com.electronwill.nbt;
+
+import com.electronwill.nbt.tagtypes.TagType;
+import com.electronwill.nbt.tagtypes.Types;
+
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * Represents an NBT tag.
+ *
+ * @param <V> the tag's value type
+ */
+public class Tag<V> {
+	protected final TagType<V> type;
+	protected String name;
+	protected V value;
+
+	public Tag(TagType<V> type, String name, V value) {
+		this.type = type;
+		this.name = name;
+		this.value = value;
+	}
+
+	public void writeNameless(DataOutput output) throws IOException {
+		output.writeByte(type.id());
+		type.writeValue(value, output);
+	}
+
+	public void writeNamed(DataOutput output) throws IOException {
+		output.writeByte(type.id());
+		Types.STRING.writeValue(name, output);
+		type.writeValue(value, output);
+	}
+
+	public TagType<V> getType() {
+		return type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public V getValue() {
+		return value;
+	}
+}
---
+++ src/main/java/com/electronwill/nbt/TagCompound.java
@@ -0,0 +1,52 @@
+package com.electronwill.nbt;
+
+import com.electronwill.nbt.tagtypes.TagType;
+import com.electronwill.nbt.tagtypes.Types;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * NBT compound tag.
+ *
+ * @author TheElectronWill
+ */
+public final class TagCompound extends Tag<Map<String, Object>> implements Iterable<Tag<?>> {
+
+	public TagCompound(String name, Map<String, Object> value) {
+		super(Types.COMPOUND, name, value);
+	}
+
+	public TagCompound(String name) {
+		super(Types.COMPOUND, name, new HashMap<String, Object>());
+	}
+
+	public Object get(String name) {
+		return value.get(name);
+	}
+
+	@Override
+	public Iterator<Tag<?>> iterator() {
+		return new Iterator<Tag<?>>() {
+			private final Iterator<Map.Entry<String, Object>> entryIterator = value.entrySet().iterator();
+
+			@Override
+			public boolean hasNext() {
+				return entryIterator.hasNext();
+			}
+
+			@Override
+			public void remove() {
+				entryIterator.remove();
+			}
+
+			@Override
+			public Tag<?> next() {
+				Map.Entry<String, Object> next = entryIterator.next();
+				TagType<Object> type = Types.get(next.getValue());
+				return new Tag<>(type, next.getKey(), next.getValue());
+			}
+		};
+	}
+}
